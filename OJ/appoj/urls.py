aefb1380b1e07078d993a89e9ae900fdbd088cc5
SayangitBIT <maitisayan24@gmail.com>
2022-06-30 20:40:08 +0530

    modifications in css

--- OJ/appoj/urls.py
+++ OJ/appoj/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from appoj.views import login, register
+from appoj.views import login_view, register
 from . import views
 
 urlpatterns = [
     # path('', views.index, name='index'),
-    path('', login.as_view()),
+    path('', login_view.as_view()),
     path('register', register.as_view()),
     path('problems', views.problems, name='problems'),
+    path('logout', views.logout_view, name='logout')
 ]

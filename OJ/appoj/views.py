690e0cab818ce644f06d8f159fbc7c0d3a1187e5
SayangitBIT <maitisayan24@gmail.com>
2022-06-30 17:06:40 +0530

    added rough login and register functionality

--- OJ/appoj/views.py
+++ OJ/appoj/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+from django.views import View
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from appoj.models import UserProfile
 
-# Create your views here.
+# def index(request):
+#     #template = loader.get_template('appoj/index.html')
+#     return render(request, 'appoj/index.html')
+
+
+class login(View):
+    def get(self, request):
+        return render(request, 'appoj/login.html')
+    def post(self, request):
+        user = authenticate(username = request.POST['username'], password = request.POST['password'])
+        if user is not None:
+            return HttpResponse("submitted yay!")
+        else:
+            return HttpResponse("invalid user")
+
+class register(View):
+    def get(self, request):
+        return render(request, 'appoj/register.html')
+    def post(self, request):
+        user = authenticate(username = request.POST['username'], password = request.POST['password'])
+        if user is None:
+            newuser = User.objects.create_user(username = request.POST['username'], password = request.POST['password'], email = request.POST['email'])
+            # print(request.POST['username'])
+            newuserprofile = UserProfile(user = newuser)
+            newuser.save()
+            newuserprofile.save()
+            return HttpResponse("registered successfully")
+        else:
+            return HttpResponse("User already present!")
+    
+def problems(request):
+    return HttpResponse("You're looking at problems list.")

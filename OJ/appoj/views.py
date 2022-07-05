253ecea3061a3da1a20ed786eb88e64c197fa45a
SayangitBIT <maitisayan24@gmail.com>
2022-07-05 08:31:50 +0530

    added basic styles to submissions and problem page

--- OJ/appoj/views.py
+++ OJ/appoj/views.py
@@ -112,7 +112,7 @@ class to_problems(LoginRequiredMixin, View):
                 _status.solved_status = "WA"
                 _status.save()
             print('Fail')
-            Submissions(problem_id_id = p_no, user_id = _user_id, _vrt = "WA").save()
+            Submissions(problem_id_id = p_no, user_id = _user_id, vrt = "WA").save()
         return redirect('/appoj/submissions')
 
 
